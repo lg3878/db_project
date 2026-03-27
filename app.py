a91509089fd8cfa6b7509a51449bf553e223991e
lg3878 <lg3878@nyu.edu>
2026-03-27 16:27:29 -0400

    Create and Read function for members table

--- app.py
+++ app.py
@@ -1,6 +1,55 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import mysql.connector
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 app = Flask(__name__)
+CORS(app)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user=DB_USERNAME,
+    password=DB_PASSWORD,
+    database="mydb"
+)
+
+cursor = db.cursor(dictionary=True)
+
+@app.route('/')
+def home():
+    return render_template('index.html', name="Gym Management System")
+
+#READ
+@app.route('/members')
+def members():
+    data = get_members()
+    return render_template('members.html', name="Members Table", rows=data)
+
+#CREATE
+@app.route('/add_member', methods=['GET','POST'])
+def add_member():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+
+        query = "INSERT INTO member(name, email, phone) VALUES (%s, %s, %s)"
+        cursor.execute(query, (name, email,phone))
+        db.commit()
+
+        return "Member added successfuly! <a href='/members'>Go back</a>"
+    return render_template('add_member.html', name='Add Member')
+
+# READ
+def get_members():
+    cursor.execute("SELECT * FROM member")
+    result = cursor.fetchall()
+    return result
 
 
 
